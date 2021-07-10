775f0222ce171237fa9208d9334f8228aa26dd5c
Chirantan Muliya <76608754+chirantan24@users.noreply.github.com>
2021-07-10 16:07:45 +0530

    Create middleelement.cpp

---
+++ LL/middleelement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head== NULL || head->next=NULL)
+            return head;
+        ListNode* sp=head->next,*fp=head->next->next;
+        while(fp && fp->next)
+        {
+            sp=sp->next;
+            fp=fp->next->next;
+        }
+        return sp;
+    }
+};
