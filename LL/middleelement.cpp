e798e2b6a753b434a7b2715f1cd4c97f079410ef
Chirantan Muliya <76608754+chirantan24@users.noreply.github.com>
2021-07-10 16:07:58 +0530

    Update middleelement.cpp

--- LL/middleelement.cpp
+++ LL/middleelement.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if(head== NULL || head->next=NULL)
+        if(head== NULL || head->next==NULL)
             return head;
         ListNode* sp=head->next,*fp=head->next->next;
         while(fp && fp->next)
