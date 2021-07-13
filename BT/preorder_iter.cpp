82aaf8be30e402eb1443559c5884ab5dd988aea3
Chirantan Muliya <76608754+chirantan24@users.noreply.github.com>
2021-07-13 14:59:22 +0530

    Create preorder_iter.cpp

---
+++ BT/preorder_iter.cpp
@@ -0,0 +1,15 @@
+void preorder(Node* root)
+{
+  stack<Node*>s;
+  s.push(root);
+  while(!s.empty())
+  {
+    Node* t=s.top();
+    s.pop();
+    cout<<t->data<<" ";
+    if(t->right)
+      s.push(t->right);
+    if(t->left)
+      s.push(t->left);
+  }
+}
